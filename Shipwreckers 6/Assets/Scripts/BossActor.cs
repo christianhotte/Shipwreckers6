dedc30c6848aa06bb81ca7b7fa24cde40a1af84c
craigslisttrashman <fintanneff@gmail.com>
2022-04-16 19:40:52 -0400

    1
    1

--- Shipwreckers 6/Assets/Scripts/BossActor.cs
+++ Shipwreckers 6/Assets/Scripts/BossActor.cs
@@ -1,51 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 [System.Serializable]
 public struct BossAction
 {
     // Animation name to be played
-    string animName;
+    public string animName;
     // Radians to be rotated
-    float rotation_min;
-    float rotation_max;
+    public float rotation_min;
+    public float rotation_max;
     // Units to move to/from player
-    float approach_min;
-    float approach_max;
+    public float approach_min;
+    public float approach_max;
     // If checked, the movement will result in an instant teleport
-    float does_teleport;
+    public bool does_teleport;
     // If checked, the 'rotation' and 'approach' coords will be absolute instead of relative
-    float move_absolute;
+    public bool move_absolute;
     // Time waited before animation plays
-    float animation_wait_time;
+    public float animation_wait_time;
     // Time waited before movement is made
-    float movement_wait_time;
+    public float movement_wait_time;
     // Time waited before another action is taken
-    float next_action_wait_time;
+    public float next_action_wait_time;
+}
+
+[System.Serializable]
+public struct BossActionSequence
+{
+    // Set of boss actions to be played in order
+    public BossAction[] set;
+    // This is the name of the sequence, it's more for debug than anything else.
+    public string sequenceName;
 }
 
 public class BossActor : MonoBehaviour, IShootable
 {
     private PolarMover pmove;
     private Animator anim;
+    [SerializeField]
+    private BossActionSequence[] actionTable;
+    private BossActionSequence[] randomActionTable;
+    private int currentSequence = 0;
+    private int currentActionInSequence = 0;
 
     [SerializeField]
-    private BossAction[][] actionTable;
+    private int health;
 
     private void Awake()
     {
         pmove = GetComponent<PolarMover>();
+        anim = GetComponent<Animator>();
+        ShuffleActionTable();
+        StartCoroutine(ShipActionLoop());
     }
 
-    private void Update()
+    private void ShuffleActionTable()
     {
-        
+        randomActionTable = actionTable.OrderBy(x => (int)Random.Range(0,9999999)).ToArray();
     }
 
-    private void PerformAction(BossAction action)
+    IEnumerator ShipActionLoop()
     {
-
+        while (health > 0)
+        {
+            ShuffleActionTable();
+            currentActionInSequence = 0;
+            while (currentActionInSequence < randomActionTable.Length)
+            {
+                Debug.Log(randomActionTable[currentActionInSequence].sequenceName);
+                currentActionInSequence++;
+                yield return new WaitForSeconds(1.0f);
+            }
+            Debug.Log("--------------");
+        }
     }
 
     public void Shoot(CannonAmmoConfig cac)
