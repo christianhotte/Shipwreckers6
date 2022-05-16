5d8759aa8454ccdd9268830a65b8204612166805
craigslisttrashman <fintanneff@gmail.com>
2022-05-15 22:42:09 -0400

    test tutorial bottle
    test tutorial bottle

---
+++ Shipwreckers 6/Assets/TutorialSound.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialSound : SipNode
+{
+    private void Awake()
+    {
+    }
+    public override void TakeSip()
+    {
+        
+    }
+}
